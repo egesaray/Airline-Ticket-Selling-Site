2ae0108e96abf76d57f89ea776c7c8e3418951c4
egesaray <73111754+egesaray@users.noreply.github.com>
2020-12-28 12:33:58 +0300

    simple reminder added

--- airline/home/models.py
+++ airline/home/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+#İrfan Ege Saray
+
 # Create your models here.
 
 #********************************************Eski deneme DB*************************************************
