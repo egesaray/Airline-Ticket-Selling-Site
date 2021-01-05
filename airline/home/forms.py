07112f3c085013d2a7c0710ac00a22e52dd701f2
egesaray <73111754+egesaray@users.noreply.github.com>
2021-01-05 21:45:55 +0300

    Update forms.py

--- airline/home/forms.py
+++ airline/home/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User , admin
 from django.contrib.auth.forms import UserCreationForm
 from home.models import *
 from django.forms import ModelForm
